c625e735668860b29195a05d3d08fc3c07ce951a
hai.xie <hai.xie@funplus.com>
2024-03-17 16:52:19 +0800

    utility

---
+++ FunkyFramework/Assets/Funky/Utility/ObjectPool.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Funky.Utility
+{
+    public class ObjectPool<T> where T : class, new()
+    {
+        private readonly Stack<T> _pool = new();
+
+        public T Allocate()
+        {
+            return _pool.Count > 0 ? _pool.Pop() : new T();
+        }
+
+        public void Recycle(T o)
+        {
+            if (o != null)
+                _pool.Push(o);
+        }
+
+        public void Clear()
+        {
+            _pool.Clear();
+        }
+
+        public int Size => _pool.Count;
+    }
+}
---
+++ FunkyFramework/Assets/Funky/Utility/Singleton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Funky.Utility
+{
+    public class Singleton<T> where T : class, new()
+    {
+        private static T _instance;
+        
+        public static T Instance
+        {
+            get
+            {
+                return _instance ??= new T();
+            }
+        }
+    }
+}
